cd3949573b40f6a5ad75f0485e99bbcf32e06294
Ukhu <Ukhu>
2023-05-09 22:14:31 -0500

    update webcal

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ let config = {
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
 							// Default, when address config is left out or empty, is "localhost"
-	port: 8080,
+	port: 80,
 	basePath: "/",			// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
 					  		// you must set the sub path here. basePath must end with a /
 	ipWhitelist: [],	// Set [] to allow all IP addresses
@@ -56,7 +56,7 @@ let config = {
 				calendars: [
 					{
 						symbol: "calendar-check",
-						url: "webcal:www.calendarlabs.com/ical-calendar/holidays/peru-holidays-484/Peru_Holidays.ics"
+						url: "webcal://www.calendarlabs.com/ical-calendar/holidays/peru-holidays-484/Peru_Holidays.ics"
 					}
 				]
 			}
