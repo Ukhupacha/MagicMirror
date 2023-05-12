5b2cf19c8341c8a62fc56c4b36ae423af11c6a6c
Ukhu <Ukhu>
2023-05-11 19:05:55 -0500

    script

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -3,6 +3,11 @@ pkg install -y vim node npm
 service sshd enable
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 service sshd start
-cd ../
+cd ~/Magicmirror
 npm run install-mm
 npm install googleapis@105 @google-cloud/local-auth@2.1.0 --save
+cd ~/Magicmirror/modules/MMM-OpenWeatherForecast
+npm install
+cp ~/Magicmirror/modules/MMM-GoogleTasks
+npm install
+cp ~/Magicmirror/modules/credentials/* ~/Magicmirror/modules/MMM-GoogleTasks/
