4ef227dd88e10c1da1e1a7ce6bd99cb872c4e1a5
bananaclient <bananaclient>
2023-09-15 16:45:33 -0500

    Update config with radio

--- config/config.js
+++ config/config.js
@@ -12,23 +12,24 @@
  * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
  */
 let config = {
-	address: "0.0.0.0",	// Address to listen on, can be:
+	address: "localhost",				// Address to listen on, can be:
+							// "0.0.0.0",
 							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
 							// Default, when address config is left out or empty, is "localhost"
-	port: 80,
-	basePath: "/",			// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+	port: 8080,					// port: 80,
+	basePath: "/",					// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
 					  		// you must set the sub path here. basePath must end with a /
-	ipWhitelist: [],	// Set [] to allow all IP addresses
+	ipWhitelist: [],				// Set [] to allow all IP addresses
 															// or add a specific IPv4 of 192.168.1.5 :
 															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
 															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
-	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
-	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
-	httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
+	useHttps: false, 				// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "", 				// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "", 				// HTTPS Certificate path, only require when useHttps is true
 
 	language: "es",
 	locale: "es-PE",
@@ -189,6 +190,23 @@ let config = {
 			}
 		},
 		{
+			module: "MMM-MplayerRadio",
+			header: "Radio",
+			position: "top_right",
+			config: {
+				customCommand: "./modules/MMM-MplayerRadio/scripts/vlcWrapper.bash",
+				customCommandArgs: ["###URL###"],
+				autoplay: 0,
+				stations: [
+					{
+						title: "Santa Monica",
+						url: "https://live.radiotv.net.pe/8042/stream",
+						logo: "https://tse1.mm.bing.net/th?id=OIP.q2zw1DOsqVzH3IFHFTyeMwHaHa&pid=Api",
+					}
+				],
+			},
+		},
+		{
 			module: 'MMM-Carousel',
 			position: 'bottom_bar',
 			config: {
@@ -197,7 +215,7 @@ let config = {
 				mode: 'slides',
 				slides : [
 					['MMM-OpenWeatherForecast', 'MMM-Senamhi'],
-					['MMM-Zkteco']
+					['MMM-Zkteco','MMM-MplayerRadio']
 				]
 
 			}
